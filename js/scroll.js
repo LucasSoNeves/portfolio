4e3beaefeb131e3a00ef011868c4428459704638
main <Lucas.neves.ead@gmail.com>
2022-10-13 14:46:33 -0300

    animações

---
+++ js/scroll.js
@@ -0,0 +1,81 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.titulo1', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 2000,
+});
+
+sr.reveal('.titulo2', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 2000,
+});
+
+sr.reveal('.titulo3', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 2000,
+});
+
+sr.reveal('.sobre', {
+    rotate: { x: 100, y: 100, z: 0},
+    duration: 2000,
+});
+
+sr.reveal('.tecno1', {
+    distance: '100px',
+    origin: 'right',
+    duration: 2000,
+});
+
+sr.reveal('.tecno2', {
+    distance: '100px',
+    origin: 'right',
+    duration: 2000,
+});
+
+sr.reveal('.tecno3', {
+    distance: '100px',
+    origin: 'right',
+    duration: 2000,
+});
+
+sr.reveal('.tecno4', {
+    distance: '100px',
+    origin: 'right',
+    duration: 2000,
+});
+
+sr.reveal('.tecno5', {
+    distance: '100px',
+    origin: 'right',
+    duration: 2000,
+});
+
+sr.reveal('.tecno6', {
+    distance: '100px',
+    origin: 'right',
+    duration: 2000,
+});
+
+sr.reveal('.projeto1', {
+    distance: '0px',
+    opacity: 0,
+    duration: 2000,
+});
+
+sr.reveal('.projeto2', {
+    distance: '0px',
+    opacity: 0,
+    duration: 2000,
+});
+
+sr.reveal('.projeto3', {
+    distance: '0px',
+    opacity: 0,
+    duration: 2000,
+});
+
+sr.reveal('.projeto4', {
+    distance: '0px',
+    opacity: 0,
+    duration: 2000,
+});
